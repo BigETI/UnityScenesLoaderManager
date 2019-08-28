f5bbf4b1b09daf44eaafd6fa68c315d4c64a6d50
BigETI <bigeti.ethem@gmail.com>
2019-08-28 13:41:02 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Scene Loader Manager for Unity
+
+## Description
+This package contains a scene loader manager for Unity.
+
+## License
+[The license](https://github.com/BigETI/UnitySceneLoaderManager/blob/master/LICENSE) for this package is available at https://github.com/BigETI/UnitySceneLoaderManager/blob/master/LICENSE
---
+++ Scripts/Managers/SceneLoaderManagerScript.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Unity scene loader manager managers namespace
+/// </summary>
+namespace UnitySceneLoaderManager.Managers
+{
+    /// <summary>
+    /// Scene loader manager script
+    /// </summary>
+    public class SceneLoaderManagerScript : MonoBehaviour
+    {
+        /// <summary>
+        /// On load scene
+        /// </summary>
+        [SerializeField]
+        private UnityEvent onLoadScene = default;
+
+        /// <summary>
+        /// On scene loaded
+        /// </summary>
+        [SerializeField]
+        private UnityEvent onSceneLoaded = default;
+
+        /// <summary>
+        /// Async operation
+        /// </summary>
+        public AsyncOperation AsyncOperation { get; private set; }
+
+        /// <summary>
+        /// Progress
+        /// </summary>
+        public float Progress
+        {
+            get
+            {
+                return ((AsyncOperation == null) ? 0.0f : AsyncOperation.progress);
+            }
+        }
+
+        /// <summary>
+        /// Instance
+        /// </summary>
+        public static SceneLoaderManagerScript Instance { get; private set; }
+
+        /// <summary>
+        /// Awake
+        /// </summary>
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        /// <summary>
+        /// Load scene
+        /// </summary>
+        public void LoadScene()
+        {
+            if (AsyncOperation == null)
+            {
+                AsyncOperation = SceneManager.LoadSceneAsync(SceneLoaderManager.SceneName);
+                if (onLoadScene != null)
+                {
+                    onLoadScene.Invoke();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        private void Update()
+        {
+            if (AsyncOperation != null)
+            {
+                if (AsyncOperation.isDone)
+                {
+                    AsyncOperation = null;
+                    if (onSceneLoaded != null)
+                    {
+                        onSceneLoaded.Invoke();
+                    }
+                }
+            }
+        }
+    }
+}
